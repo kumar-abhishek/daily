467fb113ae43174d70df74d0d4897bcfb2d2616b
Kumar Abhishek <kumar@lytmus.com>
2016-10-11 22:14:33 -0700

    one problemone problemone problemone problemone problemone problemone problemone problemone problemone problemone problem

---
+++ oct_11/66.java
@@ -0,0 +1,23 @@
+/*
+66. Plus One
+Given a non-negative number represented as an array of digits, plus one to the number.
+
+The digits are stored such that the most significant digit is at the head of the list.
+*/
+
+
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for(int i=n-1;i>=0; i--) {
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }    
+        int[] ans = new int[n+1];
+        ans[0] = 1;
+        return ans;
+    }
+}
