ba4bd4c6962f663ed9572b16786dc32c92890330
Kumar Abhishek <kumar@lytmus.com>
2016-12-05 14:04:00 -0800

    with lru

--- top50.java
+++ top50.java
@@ -1408,39 +1408,132 @@ class Solution(object):
 
 
 //LRU Cache
+ class DLinkedNode {
+    int key;
+    int value;
+    DLinkedNode pre;
+    DLinkedNode post;
+}
 
-class LRUCache:
-     def __init(self, capacity):
-         self.capacity = capacity
-         self.cache = {}
-         self.cache_vals = LinkedList()
-   
-    def set(self, key, val):
-        if self.cache.__contains__(key):
-            node = self.cache[key]
-            node.data = value
-            self.cache_vals.remove(node)
-            self.cache_vals.insert_at_tail(node)
-        else:
-            self.evict_if_needed()
-            node = LinkedListNode(key, val)
-            self.cache_vals.insert_at_tail(node)
-            self.cache[key] = node
-       
-    def get(self, key):
-        if self.cache.__contains__(key):
-            node = self.cache[key]
-            self.cache_vals.remove(node)
-            self.cache_vals.insert_at_tail(node)
-            return node.data
-        else:
-            return -1
-           
-    def evict_if_needed(self):
-        if self.cache_vals.size >= self.capacity:
-            node = self.cache_vals.remove_head()
-            del self.cache[node.key]
-           
+/**
+ * Always add the new node right after head;
+ */
+private void addNode(DLinkedNode node){
+    node.pre = head;
+    node.post = head.post;
+    
+    head.post.pre = node;
+    head.post = node;
+}
+
+/**
+ * Remove an existing node from the linked list.
+ */
+private void removeNode(DLinkedNode node){
+    DLinkedNode pre = node.pre;
+    DLinkedNode post = node.post;
+    
+    pre.post = post;
+    post.pre = pre;
+}
+
+/**
+ * Move certain node in between to the head.
+ */
+private void moveToHead(DLinkedNode node){
+    this.removeNode(node);
+    this.addNode(node);
+}
+
+// pop the current tail. 
+private DLinkedNode popTail(){
+    DLinkedNode res = tail.pre;
+    this.removeNode(res);
+    return res;
+}
+
+private Hashtable<Integer, DLinkedNode> 
+    cache = new Hashtable<Integer, DLinkedNode>();
+private int count;
+private int capacity;
+private DLinkedNode head, tail;
+
+public LRUCache(int capacity) {
+    this.count = 0;
+    this.capacity = capacity;
+
+    head = new DLinkedNode();
+    head.pre = null;
+    
+    tail = new DLinkedNode();
+    tail.post = null;
+    
+    head.post = tail;
+    tail.pre = head;
+}
+
+public int get(int key) {
+    
+    DLinkedNode node = cache.get(key);
+    if(node == null){
+        return -1; // should raise exception here.
+    }
+    
+    // move the accessed node to the head;
+    this.moveToHead(node);
+    
+    return node.value;
+}
+
+
+public void set(int key, int value) {
+    DLinkedNode node = cache.get(key);
+    
+    if(node == null){
+        
+        DLinkedNode newNode = new DLinkedNode();
+        newNode.key = key;
+        newNode.value = value;
+        
+        this.cache.put(key, newNode);
+        this.addNode(newNode);
+        
+        ++count;
+        
+        if(count > capacity){
+            // pop the tail
+            DLinkedNode tail = this.popTail();
+            this.cache.remove(tail.key);
+            --count;
+        }
+    }else{
+        // update the value.
+        node.value = value;
+        this.moveToHead(node);
+    }
+    
+}
+//Shorter using APIs
+import java.util.LinkedHashMap;
+public class LRUCache {
+    private LinkedHashMap<Integer, Integer> map;
+    private final int CAPACITY;
+    public LRUCache(int capacity) {
+        CAPACITY = capacity;
+        map = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true){
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return size() > CAPACITY;
+            }
+        };
+    }
+    public int get(int key) {
+        return map.getOrDefault(key, -1);
+    }
+    public void set(int key, int value) {
+        map.put(key, value);
+    }
+}
+
 //Hash table implementation 
 
 import java.util.ArrayList;
