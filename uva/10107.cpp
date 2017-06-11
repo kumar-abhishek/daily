9e6f37b2c313f61c776e3886dcf8409200d774a1
Kumar Abhishek <kr.abhish@gmail.com>
2017-06-11 11:31:25 -0700

    Create 10107.cpp

---
+++ uva/10107.cpp
@@ -0,0 +1,24 @@
+// accepted: 10107	What is the Median?
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n;
+	vector<int> v;
+	while(cin>>n) {
+		v.push_back(n);
+		if(v.size() % 2 == 0) {
+			nth_element(v.begin(), v.begin() + v.size()/2, v.end());
+			nth_element(v.begin(), v.begin() + v.size()/2 - 1, v.end());
+			int m1 = v[v.size()/2];
+			int m2 = v[v.size()/2 -1];
+			cout << (m1+m2)/2 << endl;
+		}
+		else {
+			nth_element(v.begin(), v.begin() + v.size()/2, v.end());
+			cout << v[v.size()/2] <<endl;
+		}
+	}
+}
